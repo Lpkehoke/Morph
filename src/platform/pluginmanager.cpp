dc5425f6ba90bcac7b962bc87ee92725b38454ec
Fedor Matantsev <fedormatantsev@gmail.com>
2019-04-09 02:07:37 +0300

    Progress

--- src/platform/pluginmanager.cpp
+++ src/platform/pluginmanager.cpp
@@ -1,26 +1,79 @@
 #include "platform/pluginmanager.h"
 
+#include "platform/knobschema.h"
+#include "platform/nodefactory.h"
+
+#include "base/registry.h"
+
+#include <functional>
+
+
 namespace platform
 {
 
+namespace
+{
+
+class NodeFactoryRegistry : public base::Registry<NodeFactoryPtr>
+{
+  public:
+    NodeFactoryRegistry()
+      : base::Registry<NodeFactoryPtr>(
+          [](const NodeFactoryPtr& factory)
+          {
+              return factory->model();
+          }
+      )
+    {}
+};
+
+class KnobSchemaRegistry : public base::Registry<KnobSchema>
+{
+  public:
+    KnobSchemaRegistry()
+      : base::Registry<KnobSchema>(
+          [](const KnobSchema& entity)
+          {
+              return entity.name();
+          })
+    {}
+};
+
+} // namespace 
+
+struct PluginManager::Impl
+{
+    NodeFactoryRegistry node_factory_registry;
+    KnobSchemaRegistry  knob_schema_registry;
+};
+
+PluginManager::PluginManager()
+  : impl(new Impl)
+{}
+
+PluginManager::~PluginManager()
+{
+    delete impl;
+}
+
 void PluginManager::register_node_factory(NodeFactoryPtr factory)
 {
-    m_node_factory_registry.register_entity(std::move(factory));
+    impl->node_factory_registry.register_entity(std::move(factory));
 }
 
 void PluginManager::register_knob_schema(KnobSchema knob_schema)
 {
-    m_knob_schema_registry.register_entity(std::move(knob_schema));
+    impl->knob_schema_registry.register_entity(std::move(knob_schema));
 }
 
 const NodeFactoryPtr& PluginManager::get_node_factory(const std::string& node_model) const
 {
-    return m_node_factory_registry.get(node_model);
+    return impl->node_factory_registry.get(node_model);
 }
 
 const KnobSchema& PluginManager::get_knob_schema(const std::string& schema_name) const
 {
-    return m_knob_schema_registry.get(schema_name);
+    return impl->knob_schema_registry.get(schema_name);
 }
 
 } // namespace platform
--- src/platform/pluginmanager.h
+++ src/platform/pluginmanager.h
@@ -1,48 +1,22 @@
 #pragma once
 
-#include "platform/knobschema.h"
-#include "platform/nodefactory.h"
-
-#include "base/registry.h"
-
-#include <functional>
 #include <memory>
 #include <string>
 
+namespace platform { class NodeFactory; }
+namespace platform { class KnobSchema; }
 
 namespace platform
 {
 
 using NodeFactoryPtr = std::shared_ptr<NodeFactory>;
 
-class NodeFactoryRegistry : public base::Registry<NodeFactoryPtr>
-{
-  public:
-    NodeFactoryRegistry()
-      : base::Registry<NodeFactoryPtr>(
-          [](const NodeFactoryPtr& factory)
-          {
-              return factory->model();
-          }
-      )
-    {}
-};
-
-class KnobSchemaRegistry : public base::Registry<KnobSchema>
-{
-  public:
-    KnobSchemaRegistry()
-      : base::Registry<KnobSchema>(
-          [](const KnobSchema& entity)
-          {
-              return entity.name();
-          })
-    {}
-};
-
 class PluginManager
 {
   public:
+    PluginManager();
+    ~PluginManager();
+
     void register_node_factory(NodeFactoryPtr factory);
     void register_knob_schema(KnobSchema knob_schema);
 
@@ -50,8 +24,8 @@ class PluginManager
     const KnobSchema& get_knob_schema(const std::string& schema_name) const;
 
   private:
-    NodeFactoryRegistry m_node_factory_registry;
-    KnobSchemaRegistry m_knob_schema_registry;
+    struct Impl;
+    Impl* impl;
 };
 
 } // namespace platform
